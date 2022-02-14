0817616ba9d1970ef5ffe3aa01b1fca9c6168775
Andrei <sp071@mail.ru>
2022-02-14 06:39:09 +0300

    FIRST commit

---
+++ JS/SCRIPT.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`hello, ${user}`);
+
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = prompt("вам есть 18?", "пиши правду");
+// console.log(answer);
+
+// const answers = []
+
+// answers[0] = prompt("как ваше имя?", "");
+// answers[1] = prompt("как ваша фамилия?", "");
+// answers[2] = prompt("сколько вам лет?", "");
+
+// document.write(answers);
+
+
+
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const person = 'Alex';
+
+const bool = true;
+
+
+let und;
+console.log(und);
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 6, {}, []];
+console.log(arr[1]);
